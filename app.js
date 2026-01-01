49e5d574da665108f0d30bf1e4d849b0cfe5e466
Ojo Jeremiah <ojojeremiah249@gmail.com>
2026-01-01 19:20:32 +0100

    Final portfolio build

--- app.js
+++ app.js
@@ -44,4 +44,6 @@ app.post('/contact', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
